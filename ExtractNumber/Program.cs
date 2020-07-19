e84b6e1dd014de00117ab09adea8c8b9692245f5
wendee <b05705046@ntu.edu.tw>
2020-07-19 14:58:09 +0800

    add .gitignore

---
+++ ExtractNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtractNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
